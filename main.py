c9d235ec17bc6dc8fecbc590c3829fb87c7c0b23
Tapeline <yt.redstone.mail@gmail.com>
2024-12-01 12:29:20 +0500

    My commit 3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    # Мой комментарий 2
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
